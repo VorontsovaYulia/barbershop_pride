acb1030f151bd2b63a93214b45d99bf73695a23c
VorontsovaYulia <vorontsovayulia89@gmail.com>
2025-04-17 16:30:47 +0300

    added_h2

--- app/components/Gallery.js
+++ app/components/Gallery.js
@@ -1,5 +1,8 @@
 export const Gallery = () => {
     return (
-        <section>Gallery</section>
+        <section>
+            <h2 className="block text-center text-[22px]/[30px] italic xl:text-[36px]/[42px]">ГАЛЕРЕЯ</h2>
+
+        </section>
     )
 }
