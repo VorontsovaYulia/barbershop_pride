90f51fb1146c19a899d45a204b4abb82e78b067b
VorontsovaYulia <vorontsovayulia89@gmail.com>
2025-04-20 03:17:51 +0300

    added_ currentSlide

--- app/components/Gallery.js
+++ app/components/Gallery.js
@@ -1,11 +1,11 @@
 'use client';
 
-import Image from 'next/image';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { useRef, useEffect, useState } from 'react';
 import { BsArrowRight, BsArrowLeft } from "react-icons/bs";
+import { useRef, useEffect, useState } from 'react';
+import Image from 'next/image';
 
 const slides = [
   '/slider/slide1.jpg',
@@ -20,12 +20,11 @@ const slides = [
 export const Gallery = () => {
     const sliderRef = useRef(null);
     const [isMobile, setIsMobile] = useState(false);
+    const [currentSlide, setCurrentSlide] = useState(2);
+    const arrowSize = isMobile ? 32 : 40;
 
     useEffect(() => {
-        const checkSize = () => {
-            setIsMobile(window.innerWidth < 1440);
-        };
-
+        const checkSize = () => setIsMobile(window.innerWidth < 1440);
         checkSize();
         window.addEventListener('resize', checkSize);
         return () => window.removeEventListener('resize', checkSize);
@@ -42,7 +41,10 @@ export const Gallery = () => {
         infinite: true,
         arrows: false,
         speed: 500,
-        slidesToShow: isMobile ? 1.06 : 4.35,
+        slidesToShow: isMobile ? 1.06 : 4.34,
+        beforeChange: (_, newIndex) => {
+            setCurrentSlide(newIndex);
+        },
         responsive: [
             {
                 breakpoint: 1440,
@@ -55,45 +57,49 @@ export const Gallery = () => {
 
     return (
         <section className="py-10 xl:pt-20 xl:pb-[106px]">
-            <h2 className="block text-center mb-6 xl:mb-10 italic text-[22px]/[30px] xl:text-[36px]/[42px] ">
+
+            <h2 className="block text-center mb-6 xl:mb-14 italic text-[22px]/[30px] xl:text-[36px]/[42px]">
                 ГАЛЕРЕЯ
             </h2>
 
-            <div className="slider-container relative mx-auto max-w-[1440px]">
+            <div className="slider-container relative mx-auto max-w-[1440px] overflow-x-clip">
+                <style jsx global>{`div .slick-list {overflow: visible}`}</style>
+
                 <Slider {...settings} ref={sliderRef}>
-                    {slides.map((slide, i) => (
-                        <div
-                            key={i}
-                            className={`shrink-0 ${isMobile ? 'px-2' : 'px-3'}`}
-                        >
-                            <div className="w-full">
-                                <Image
-                                    src={slide}
-                                    alt={`Слайд ${i + 1}`}
-                                    width={isMobile ? 338 : 308}
-                                    height={isMobile ? 462 : 421}
-                                    priority
-                                />
+                    {slides.map((slide, i) => {
+                        const isActive = i === currentSlide;
+
+                        return (
+                            <div
+                                key={i}
+                                className={`shrink-0 ${isMobile ? 'px-2' : 'px-3'} flex justify-center`}
+                            >
+                                <div
+                                    className={`transition-all duration-500 ${!isMobile && isActive ? 'scale-[1.1] z-10' : 'scale-100 z-0'}`}
+                                >
+                                    <Image
+                                        src={slide}
+                                        alt={`Слайд ${i + 1}`}
+                                        width={isMobile ? 338 : 308}
+                                        height={isMobile ? 462 : 421}
+                                        priority
+                                    />
+                                </div>
                             </div>
-                        </div>
-                    ))}
+                        );
+                    })}
                 </Slider>
 
-                <div className="flex justify-between items-center mt-2 xl:mt-6 px-4 xl:px-18 h-8 xl:h-10">
-                    <button
-                        onClick={handlePrev}
-                        aria-label="стрілка назад"
-                    >
-                        {isMobile ? <BsArrowLeft size={32} className="fill-white"/> : <BsArrowLeft size={40} className="fill-white"/>}
+                <div className="flex justify-between items-center mt-2 xl:mt-8 px-4 xl:px-18 h-8 xl:h-10">
+                    <button onClick={handlePrev} aria-label="стрілка назад">
+                        <BsArrowLeft size={arrowSize} className="fill-white hover:fill-hover active:fill-main" />
                     </button>
-                    <span className="text-center text-sm xl:text-base italic text-white">БІЛЬШЕ</span>
-                    <button
-                        onClick={handleNext}
-                        aria-label="стрілка вперед"
-                    >
-                        {isMobile ? <BsArrowRight size={32} className="fill-white"/> : <BsArrowRight size={40} className="fill-white"/>}
+                    <span className="cursor-pointer text-center text-sm xl:text-base italic text-white">БІЛЬШЕ</span>
+                    <button onClick={handleNext} aria-label="стрілка вперед">
+                        <BsArrowRight size={arrowSize} className="fill-white hover:fill-hover active:fill-main" />
                     </button>
                 </div>
+
             </div>
         </section>
     );
