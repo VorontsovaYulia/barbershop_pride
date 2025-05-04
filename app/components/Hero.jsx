62eca6b1d927678faf06075c68358937479345af
Va7ul <vas7ul@gmail.com>
2025-05-04 21:42:41 +0300

    Update Hero.jsx

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -13,7 +13,7 @@ export const Hero = async () => {
         alt="Hero Background"
         fill
         priority
-        quality={70}
+        quality={65}
         className="block object-cover xl:hidden"
         sizes="(max-width: 1440px) 100vw"
       />
@@ -21,7 +21,6 @@ export const Hero = async () => {
         src={HeroImg}
         alt="Hero Background"
         fill
-        quality={70}
         className="hidden object-cover xl:block"
         sizes="(min-width: 1440px) 100vw"
       />
