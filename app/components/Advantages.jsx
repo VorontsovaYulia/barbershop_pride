a625c87280b22527c8b3a724ab1a3c7f01753f38
NatalyaMaterinskaya <natulka-91@ukr.net>
2025-05-05 16:30:00 +0300

    English language added to Barbers

--- app/components/Advantages.jsx
+++ app/components/Advantages.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 export const Advantages = async () => {
   const t = await getTranslations('Advantages');
-console.log(t.raw('advantage'));
+
   return (
     <section className="px-4 pt-12 pb-8 xl:px-18 xl:pt-20 xl:pb-10">
       <h2 className="mb-10 text-center text-[22px]/[30px] uppercase italic xl:mb-16 xl:text-[36px]/[42px]">
@@ -12,7 +12,7 @@ console.log(t.raw('advantage'));
       </h2>
       <div className="flex items-center justify-center">
         <ul className="flex  max-w-60 flex-col gap-y-10 xl:min-w-212 xl:flex-row xl:justify-between">
-          {t.raw('advantage').map((item, idx) => (
+          {t.raw('advantages').map((item, idx) => (
             <li
               key={idx}
               className="flex flex-col items-center gap-y-4 xl:gap-y-6"
--- app/components/Barbers.jsx
+++ app/components/Barbers.jsx
@@ -1,74 +1,29 @@
+import { getTranslations } from 'next-intl/server';
 import Image from 'next/image';
 import Link from 'next/link';
 
-const barbers = [
-  {
-    image_mob: '/images/barbers-Bohdan-mob.jpg',
-    image_desk: '/images/barbers-Bohdan-desk.jpg',
-    name: 'Богдан',
-    image_hover_mob: '/images/barbers-Bohdan-mob-hover.jpg',
-    image_hover_desk: '/images/barbers-Bohdan-desk-hover.jpg',
-    text_hover: 'Від класики до трендів',
-    description_for_img: 'Фото майстра Богдана',
-    icon: 'icons/barbers-altegio.svg',
-    link: '',
-  },
-  {
-    image_mob: '/images/barbers-Kate-mob.jpg',
-    image_desk: '/images/barbers-Kate-desk.jpg',
-    name: 'Катерина',
-    image_hover_mob: '/images/barbers-Kate-mob-hover.jpg',
-    image_hover_desk: '/images/barbers-Kate-desk-hover.jpg',
-    text_hover: 'Твій новий стиль уже в її голові',
-    description_for_img: 'Фото майстра Катерини',
-    icon: 'icons/barbers-altegio.svg',
-    link: '',
-  },
-  {
-    image_mob: '/images/barbers-Dima-mob.jpg',
-    image_desk: '/images/barbers-Dima-desk.jpg',
-    name: 'Дмитро',
-    image_hover_mob: '/images/barbers-Dima-mob-hover.jpg',
-    image_hover_desk: '/images/barbers-Dima-desk-hover.jpg',
-    text_hover: 'Точність у кожному русі',
-    description_for_img: 'Фото майстра Дмитра',
-    icon: 'icons/barbers-altegio.svg',
-    link: '',
-  },
-  {
-    image_mob: '/images/barbers-Darina-mob.jpg',
-    image_desk: '/images/barbers-Darina-desk.jpg',
-    name: 'Дарина',
-    image_hover_mob: '/images/barbers-Darina-mob-hover.jpg',
-    image_hover_desk: '/images/barbers-Darina-desk.jpg',
-    text_hover: '?????????????????????',
-    description_for_img: 'Фото майстра Дарини',
-    icon: 'icons/barbers-altegio.svg',
-    link: '',
-  },
-];
-
-export const Barbers = () => {
+export const Barbers = async() => {
+    const t = await getTranslations('Barbers');
   return (
     <section className="px-4 pt-10 pb-10 xl:px-18 xl:pt-12 xl:pb-28">
       <h2 className="mb-10 text-center text-[22px]/[30px] uppercase italic xl:mb-16 xl:text-[36px]/[42px]">
-        Наші майстри
+        {t('title')}
       </h2>
       <div className="flex items-center justify-end pr-2 pl-6 xl:justify-center xl:xl:pr-0 xl:pl-0">
         <ul className="flex flex-col gap-y-6 xl:w-228 xl:flex-row xl:flex-wrap xl:justify-center xl:gap-x-44 xl:gap-y-12">
-          {barbers.map((item, idx) => (
+          {t.raw('barbers').map((item, idx) => (
             <li
               key={idx}
               className="group relative flex w-78 flex-col items-end gap-y-1 xl:w-92 xl:gap-y-4 xl:nth-last-2:order-last"
             >
               <Link
                 href="/"
-                className="group pointer-events-none flex h-88 w-78 justify-end xl:h-108 xl:w-92 xl:pointer-events-auto"
+                className="group pointer-events-none flex h-88 w-78 justify-end xl:pointer-events-auto xl:h-108 xl:w-92"
               >
                 {/* Рамка */}
                 <span className="mr-4 h-78 w-60 border-1 xl:mr-1 xl:h-95 xl:w-73"></span>
                 {/* Світлий бекдроп */}
-                <div className="xl: absolute top-6 right-12 z-20 h-81 w-66 bg-[#EACCB6]/10 transition-opacity duration-500 ease-in-out group-hover:opacity-0 group-focus:opacity-0  group-active:opacity-0 xl:top-8 xl:right-10 xl:h-100 xl:w-80"></div>
+                <div className="xl: absolute top-6 right-12 z-20 h-81 w-66 bg-[#EACCB6]/10 transition-opacity duration-500 ease-in-out group-hover:opacity-0 group-focus:opacity-0 group-active:opacity-0 xl:top-8 xl:right-10 xl:h-100 xl:w-80"></div>
                 {/* Темний бекдроп */}
                 <div className="xl: absolute top-6 right-12 z-20 h-81 w-66 bg-[#000000]/30 opacity-0 transition-opacity duration-500 ease-in-out group-hover:opacity-100 group-focus:opacity-100 group-active:opacity-100 xl:top-8 xl:right-10 xl:h-100 xl:w-80">
                   <p className="k absolute top-4 z-40 w-66 text-center text-[14px]/[22px] font-normal text-white opacity-0 transition-opacity duration-500 ease-in-out group-hover:opacity-100 group-focus:opacity-100 group-active:opacity-100 xl:w-80 xl:text-[16px]/[22px]">
@@ -88,7 +43,7 @@ export const Barbers = () => {
                   src={item.image_hover_mob}
                   width={264}
                   height={324}
-                  className="absolute top-6 right-12 hidden group-hover:block group-focus:block group-active:block  xl:hidden xl:group-hover:hidden xl:group-focus:hidden"
+                  className="absolute top-6 right-12 hidden group-hover:block group-focus:block group-active:block xl:hidden xl:group-hover:hidden xl:group-focus:hidden"
                   alt={item.description_for_img}
                 />
 
