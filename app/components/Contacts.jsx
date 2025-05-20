1f1280465b0a6cfc31c46b54c7c3fc4c718575f9
VorontsovaYulia <vorontsovayulia89@gmail.com>
2025-05-20 20:41:09 +0300

    added_ address

--- app/components/Contacts.jsx
+++ app/components/Contacts.jsx
@@ -14,8 +14,8 @@ export const Contacts = async () => {
             <LuMapPin className="h-5 w-5 cursor-default xl:h-7 xl:w-7" />
 
             <div>
-              <p className="block">{t('RCaddress')}</p>{' '}
-              <p className="block">{t('address')}</p>
+              <address className="block">{t('RCaddress')}</address>{' '}
+              <address className="block">{t('address')}</address>
             </div>
           </li>
           <li className="flex items-center gap-4">
